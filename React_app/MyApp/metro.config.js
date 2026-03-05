00ac8c90de4c8ef26dcce3417f8039a142c4ec6e
Mayurishetty-04 <mayurishetty444@gmail.com>
2026-03-05 10:26:20 +0530

    Triage model

---
+++ React_app/MyApp/metro.config.js
@@ -0,0 +1,20 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push('wasm');
+config.resolver.sourceExts.push('wasm');
+
+config.server = {
+    enhanceMiddleware: (middleware) => {
+        return (req, res, next) => {
+            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+            return middleware(req, res, next);
+        };
+    },
+};
+
+module.exports = config;
